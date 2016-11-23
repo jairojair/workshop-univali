dfcd87af7c583660900254b90b91e667bf3382e1
Jairo Jair <jairojair@dev.local>
2016-11-23 19:21:05 -0200

    update submarino

--- exemplos/busca/submarino.py
+++ exemplos/busca/submarino.py
@@ -5,4 +5,9 @@ import requests
 url = 'http://submarino.com.br/produto/120166881'
 resposta = requests.get(url)
 pagina = BeautifulSoup(resposta.content, "html.parser")
-print(pagina.find_all('p', {'itemprop': 'price'})[0].text)
+
+name  = pagina.find_all('h1', {'class': 'product-name'})[0].text
+price = pagina.find_all('p', {'itemprop': 'price'})[0].text
+
+print(name)
+print(price)
